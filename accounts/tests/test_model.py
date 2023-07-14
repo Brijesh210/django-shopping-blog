8852147b757029e6a5ee6515d706a4ccc28e7f1c
Brijesh Varsani <varsani340@gmail.com>
2023-07-14 22:06:11 +0200

    django-pytest, pytest fixture

--- accounts/tests/test_model.py
+++ accounts/tests/test_model.py
@@ -5,6 +5,7 @@ from accounts.tests.factories import UserFactory
 # Here, created own fixture, in confest.py, so pytest find that fixture and use it everywehre needed
 # @pytest.mark.django_db
 class TestUser:
+
     # multiple model in signle user app
     # factory is tool to make test data for model
     # test real database, create the database layer, when test running, and mock out the complelate database,
